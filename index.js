606f9e52a19948a2f597eea60858b44fe44a2209
Alex Levine <aalexlevine@gmail.com>
2017-09-21 02:58:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,5 +29,4 @@ var funkyFunction = function() {
 // NOTE: you only need to modify the code below this line.
 
 var theFunk = funkyFunction()
-funkyFunction = thefunk
 //How would we tell that function to execute to return the string within it?
